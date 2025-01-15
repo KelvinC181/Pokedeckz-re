2d9e0b2e6017efd09f7cf859af8839bfec7b989b
Kelvin Chan <kchan.dbs@gmail.com>
2025-01-15 14:12:57 +0000

    create Card model

--- card/models.py
+++ card/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+# Create your models here.
+class Card(models.Model):
+    card_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=200)
+    card_image = CloudinaryField('image', default='placeholder')
