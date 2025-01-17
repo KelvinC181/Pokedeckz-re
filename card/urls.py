d9fa41d33a885947c0277400546ac954a903fe8c
Kelvin Chan <kchan.dbs@gmail.com>
2025-01-17 14:16:09 +0000

    add view and url path for card detail

--- card/urls.py
+++ card/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.CardLibrary.as_view(), name='home'),
+    path('', views.CardLibrary.as_view(), name="home"),
+    path("<str:card_id>/", views.card_detail, name="event_detail")
 ]
--- card/views.py
+++ card/views.py
@@ -1,8 +1,38 @@
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Card
 
 # Create your views here.
 
+
 class CardLibrary(generic.ListView):
+    """
+    Dispalys the card library
+    """
     queryset = Card.objects.all()
     template_name = "card/card_library.html"
+
+
+def card_detail(request, card_id):
+    """
+    Display an individual :model:`card.Card`.
+
+    **Context**
+
+    ``card``
+        An instance of :model:`card.Card`.
+
+    **Template:**
+
+    :template:`card/card_detail.html`
+    """
+    queryset = Card.objects.all()
+    card = get_object_or_404(queryset, card_id=card_id)
+
+    return render(
+        request,
+        "card/card_detail.html",
+        {"card": card},
+    )
