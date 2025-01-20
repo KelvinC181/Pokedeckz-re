0e4c85c6d8ae878467d765458fab5c51ddd446c3
Kelvin Chan <kchan.dbs@gmail.com>
2025-01-20 15:00:20 +0000

    install summernotes

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cloudinary_storage',
+    'django_summernote',
     'cloudinary',
     'card',
     'deckbuilder',
