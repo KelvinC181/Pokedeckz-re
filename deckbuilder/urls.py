7caa5f32bfc0c8b460836308eea55af653107344
Kelvin Chan <kchan.dbs@gmail.com>
2025-01-24 15:11:20 +0000

    add deck_detail url

--- deckbuilder/urls.py
+++ deckbuilder/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.deckbuilder, name='deckbuilder'),
+    path('decks/', views.DeckList.as_view(), name='deck_list'),
     path('my-decks/', views.MyDecks.as_view(), name='my_decks'),
     path('<int:id>/', views.deck_detail, name="deck_detail"),
 ]
