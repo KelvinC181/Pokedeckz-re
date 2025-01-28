6feb8b2e38440862c274470f1b2c4910c367add4
Kelvin Chan <kchan.dbs@gmail.com>
2025-01-28 14:53:34 +0000

    add missing semi-colons

--- static/js/deckbuilder.js
+++ static/js/deckbuilder.js
@@ -21,16 +21,15 @@ document.addEventListener('DOMContentLoaded', function() {
             deleteDeckModal.classList.add('hidden');
         });
     }
-})
+});
 
 const cardImgs = document.querySelectorAll('.library-img');
 const textarea = document.querySelector('textarea[name="deck_content"]');
-const deckarea = document.querySelector('.deck-area')
+const deckarea = document.querySelector('.deck-area');
 const deleteDeckButton = document.querySelector('.delete-deck');
 const closeButtons = document.querySelectorAll('.close-button');
-const deckAreaImgs = document.querySelectorAll('.deck-img')
+const deckAreaImgs = document.querySelectorAll('.deck-img');
 const deleteDeckModal = document.getElementById('delete-modal');
-const deleteConfirm = document.getElementById('delete-confirm')
 
 /**
  * ADD card to deck
@@ -55,16 +54,16 @@ const deleteConfirm = document.getElementById('delete-confirm')
 //2. add img to deck area dynamically
 const addCardImg = (cardId, cardImgSrc, cardImgAlt) => {
     //create card div
-    let cardDiv = document.createElement('div')
-    cardDiv.classList.add('col-12', 'col-md-3', 'col-lg-2', 'my-2')
+    let cardDiv = document.createElement('div');
+    cardDiv.classList.add('col-12', 'col-md-3', 'col-lg-2', 'my-2');
 
 
     //create img 
-    let deckImg = document.createElement('img')
-    deckImg.classList.add('deck-img')
-    deckImg.setAttribute('src', cardImgSrc)
-    deckImg.setAttribute('card-id', cardId)
-    deckImg.setAttribute('alt', cardImgAlt)
+    let deckImg = document.createElement('img');
+    deckImg.classList.add('deck-img');
+    deckImg.setAttribute('src', cardImgSrc);
+    deckImg.setAttribute('card-id', cardId);
+    deckImg.setAttribute('alt', cardImgAlt);
 
     //append img to div
     cardDiv.appendChild(deckImg);
@@ -74,15 +73,15 @@ const addCardImg = (cardId, cardImgSrc, cardImgAlt) => {
 
     //add event listener for deletecard fuction
     deckImg.addEventListener('click', () => deleteCard(deckImg));
-}
+};
 
 
 //1. append card to deck content field
 const addCard = (cardImg) => {
     // Get the card ID/src/alt
     let cardId = cardImg.getAttribute('card-id');
-    let cardImgSrc = cardImg.getAttribute('src')
-    let cardImgAlt = cardImg.getAttribute('alt')
+    let cardImgSrc = cardImg.getAttribute('src');
+    let cardImgAlt = cardImg.getAttribute('alt');
 
     // Get content of deck_content
     let deckContent = textarea.value;
@@ -105,14 +104,14 @@ const addCard = (cardImg) => {
             // append card ID to text area
             deckArray.push(cardId);
             textarea.value = deckArray.join(',');
-            addCardImg (cardId, cardImgSrc, cardImgAlt)
+            addCardImg (cardId, cardImgSrc, cardImgAlt);
         } else {
-            alert('deck can only contain 20 cards')
+            alert('deck can only contain 20 cards');
             return;
         }
     }
     
-}
+};
 
 
 /**
@@ -138,16 +137,16 @@ const deleteCard = (deckImg) => {
     let deckArray = deckContent.split(',');
 
     //get index of id in array
-    let idPosition = deckArray.indexOf(cardId)
+    let idPosition = deckArray.indexOf(cardId);
 
     //splice id from array
     if (idPosition >-1) {
-        deckArray.splice(idPosition,1)
+        deckArray.splice(idPosition,1);
     }
-    textarea.value = deckArray.join(',')
+    textarea.value = deckArray.join(',');
 
-    console.log(textarea.value)
+    console.log(textarea.value);
 
     //2. delete img from deck area dynamically
     deckImg.parentElement.remove();
-}
+};
